2e30d853afbb628483f515d59dc92cfc4e58c9ef
shilin <shilhu@cs.stonybrook.edu>
2021-05-16 11:42:54 -0400

    BDRAR

---
+++ BDRAR/config.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+sbu_training_root = '/nfs/bigfovea/add_disk0/shilinhu/SBU/SBUTrain4KRecoveredSmall'
+sbu_testing_root = '/nfs/bigfovea/add_disk0/shilinhu/SBU/SBU-Test'
+cuhk_training_root = '/data/add_disk0/shilinhu/CUHKshadow/train'
+cuhk_testing_root = '/data/add_disk0/shilinhu/CUHKshadow/test'
